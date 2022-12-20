3c2ecdbc3bd965122e5e7d7fcb38670bd7c290bd
Igor Wawrzyniak <igorwaw@gmail.com>
2022-12-20 18:43:17 +0100

    code cleanup

--- 03-houses.py
+++ 03-houses.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 
-from dataclasses import dataclass
 INPUTFILE="03-input.txt"
 
 
 
-numsantas=2 # how many Santas
-counter=0 # instruction counter
-
-@dataclass
 class Santa:
     x: int
     y: int
-    visited: set[int,int]
+    visited: 'set[int,int]'
     
     def __init__(self, x=0, y=0):
         self.x=0
@@ -25,36 +20,36 @@ class Santa:
         self.visited.add( (self.x, self.y) )
 
 
+def santatour(numsantas: int=1):
+    counter=0 # instruction counter
+    santas = [ Santa() for i in range(numsantas)]
+    with open(INPUTFILE) as inputfile:
+        while True:
+            s=counter%numsantas
+            c=inputfile.read(1)
+            #print(f"Counter {counter} instruction {c} santa {s}")
+            if not c:
+                break
+            if   c=='<': santas[s].x-=1
+            elif c=='>': santas[s].x+=1
+            elif c=='^': santas[s].y+=1
+            elif c=='v': santas[s].y-=1
+            else:
+                print("Error: unkown direction ",c)
+            santas[s].record_visit()
+            counter+=1
+    print ("Houses visited by each Santa")
+    totalvisitsnum=0
+    totalvisited=set()
+    for i in range(numsantas):
+        visits=len(santas[i].visited)
+        totalvisited=totalvisited|santas[i].visited
+        print(f"  Santa {i}  houses {visits}")
+        #print(santas[i])
+    print("Total: ", len(totalvisited))
 
+print("Part 1:")
+santatour(1)
 
-santas: list[Santa] = []
-for i in range(0, numsantas):
-    santas.append( Santa() )
-
-
-with open(INPUTFILE) as inputfile:
-    while True:
-        s=counter%numsantas
-        c=inputfile.read(1)
-        #print(f"Counter {counter} instruction {c} santa {s}")
-        if not c:
-            break
-        if   c=='<': santas[s].x-=1
-        elif c=='>': santas[s].x+=1
-        elif c=='^': santas[s].y+=1
-        elif c=='v': santas[s].y-=1
-        else:
-            print("Error: unkown direction ",c)
-        santas[s].record_visit()
-        counter+=1
-
-
-print ("Houses visited by each Santa")
-totalvisitsnum=0
-totalvisited=set()
-for i in range(0, numsantas):
-    visits=len(santas[i].visited)
-    totalvisited=totalvisited|santas[i].visited
-    print(f"  Santa {i}  houses {visits}")
-    #print(santas[i])
-print("Total: ", len(totalvisited))
+print("\n\nPart 2:")
+santatour(2)
