210c669ee9daa0de0d2f302a8fd66e1f8ae86aaf
Igor Wawrzyniak <igorwaw@gmail.com>
2023-01-04 11:31:44 +0100

    day 16 done

---
+++ day15-19/16-sue.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import re
+
+INPUTFILE="16-input.txt"
+
+
+aunt_sender= {
+"children": 3,
+"cats": 7,
+"samoyeds": 2,
+"pomeranians": 3,
+"akitas": 0,
+"vizslas": 0,
+"goldfish": 5,
+"trees": 3,
+"cars": 2,
+"perfumes": 1
+}
+
+
+
+# read file into list
+aunts=[]
+rx=re.compile( r"Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)" )
+with open(INPUTFILE) as inputfile:
+    for line in inputfile:
+        number, item1, item1value, item2, item2value, item3, item3value  = rx.match(line).groups()
+        aunts.append( ( (item1, int(item1value)), (item2, int(item2value)), (item3, int(item3value) )) )
+
+# part 1
+for i,aunt in enumerate(aunts):
+    itemsmatched=0
+
+    for item in aunt:
+        if item[1] == aunt_sender[item[0]]:
+            itemsmatched+=1
+        else:
+            break # no need to check other items
+    if itemsmatched==3:
+        print(f"Part 1: aunt {i+1} matches") 
+        break
+
+
+# part 2
+for i,aunt in enumerate(aunts):
+    itemsmatched=0
+
+    for item in aunt:
+        if item[0]=="cats" or item[0]=="trees":
+            if item[1] > aunt_sender[item[0]]:    
+                itemsmatched+=1
+        elif item[0]=="pomeranians" or item[0]=="goldfish":
+            if item[1] < aunt_sender[item[0]]:    
+                itemsmatched+=1
+        else:
+            if item[1] == aunt_sender[item[0]]:
+                itemsmatched+=1
+    if itemsmatched==3:
+        print(f"Part 2: aunt {i+1} matches") 
+        break
+
--- day15-19/README.md
+++ day15-19/README.md
@@ -7,3 +7,9 @@ I added a progress bar with tqdm because an early version took minutes and I wan
 it's progressing at all. Turned out that an improved version only needs a second so it's not
 really necessary. I made sure I'm filtering the list of combinations before generating
 permutations and that I'm not storing more data than needed.
+
+# Day 16: Aunt Sue
+
+Appareantly, I have 500 aunts names Sue, some of which have a small herd of pomeranians and samoyeds.
+I like the story. The puzzle itself is not that interesting, just some basic input file processing
+with regexps (split() would also work) and few comparisons.
